b695629b19b549da3e13b86f63d6690e51f2ccb7
zecho <zeecho@users.noreply.github.com>
2024-04-08 22:46:55 +0200

    Increase services tests coverage

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTests.java
@@ -17,6 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.SessionRepository;
 import com.openclassrooms.starterjwt.repository.UserRepository;
 
@@ -38,16 +39,12 @@ class SessionServiceTests {
     @Test
     @DisplayName("Create a new session (checkink it's not null and has the right name")
     public void testCreate() {
-        // Mock data
         Session session = new Session(1L, "Test Session", new Date(), "Description", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now());
 
-        // Mock the repository behavior
         when(sessionRepository.save(session)).thenReturn(session);
 
-        // Call the service method
         Session result = sessionService.create(session);
 
-        // Assertions
         assertThat(result).isNotNull();
         assertThat(result.getName()).isEqualTo("Test Session");
     }
@@ -55,18 +52,14 @@ class SessionServiceTests {
     @Test
     @DisplayName("Getting all sessions (comparing size of array and some data)")
     public void testFindAll() {
-        // Mock data
         List<Session> sessions = new ArrayList<>();
         sessions.add(new Session(1L, "Test Session 1", new Date(), "Description 1", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now()));
         sessions.add(new Session(2L, "Test Session 2", new Date(), "Description 2", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now()));
 
-        // Mock the repository behavior
         when(sessionRepository.findAll()).thenReturn(sessions);
 
-        // Call the service method
         List<Session> result = sessionService.findAll();
 
-        // Assertions
         assertThat(result.size()).isEqualTo(2);
         assertThat(result.get(0).getName()).isEqualTo("Test Session 1");
         assertThat(result.get(1).getDescription()).isEqualTo("Description 2");
@@ -75,17 +68,13 @@ class SessionServiceTests {
     @Test
     @DisplayName("Retriving an existing Session through the service")
     public void testGetById_ExistingId_ReturnsSession() {
-        // Mock data
         Long sessionId = 1L;
         Session session = new Session(sessionId, "Test Session", new Date(), "Description", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now());
 
-        // Mock the repository behavior
         when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(session));
         
-        // Call the service method
         Session result = sessionService.getById(sessionId);
 
-        // Assertions
         assertThat(result).isNotNull();
         assertThat(result.getDescription()).isEqualTo("Description");
     }
@@ -93,13 +82,68 @@ class SessionServiceTests {
     @Test
     @DisplayName("Checking we get NULL when trying to retrieve a non-existing session")
     public void testGetById_NonExistingId_ReturnsNull() {
-        // Mock the repository behavior
         when(sessionRepository.findById(999L)).thenReturn(Optional.empty());
         
-        // Call the service method
         Session result = sessionService.getById(999L);
 
-        // Assertions
         assertThat(result).isNull();
+    }
+    
+    @Test
+    @DisplayName("Delete an existing session")
+    void testDelete_ExistingSession_DeletesSession() {
+        Long sessionId = 1L;
+        
+        sessionService.delete(sessionId);
+        
+        verify(sessionRepository).deleteById(sessionId);
+    }
+    
+
+    @Test
+    @DisplayName("Update an existing session")
+    void testUpdate_ExistingSession_UpdatesSession() {
+        Long sessionId = 1L;
+        Session updatedSession = new Session(sessionId, "Updated Session", new Date(), "Updated Description", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now());
+        
+        when(sessionRepository.save(any(Session.class))).thenReturn(updatedSession);
+        
+        Session result = sessionService.update(sessionId, updatedSession);
+        
+        assertThat(result).isEqualTo(updatedSession);
+    }
+    
+    @Test
+    @DisplayName("Participate in a session")
+    void testParticipate_InExistingSession_ParticipatesInSession() {
+        Long sessionId = 1L;
+        Long userId = 1L;
+        Session session = new Session(sessionId, "Test Session", new Date(), "Description", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now());
+        User user = new User(userId, "user@example.com", "John", "Doe", "password", false, LocalDateTime.now(), LocalDateTime.now());
+        
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(session));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+        
+        sessionService.participate(sessionId, userId);
+        
+        verify(sessionRepository).save(session);
+    }
+    
+    @Test
+    @DisplayName("No longer participate in a session")
+    void testNoLongerParticipate_InExistingSession_NoLongerParticipatesInSession() {
+        Long sessionId = 1L;
+        Long userId = 1L;
+        Session session = new Session(sessionId, "Test Session", new Date(), "Description", null, new ArrayList<>(), LocalDateTime.now(), LocalDateTime.now());
+        User user = new User(userId, "user@example.com", "John", "Doe", "password", false, LocalDateTime.now(), LocalDateTime.now());
+        session.getUsers().add(user);
+        
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(session));
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+        
+        sessionService.noLongerParticipate(sessionId, userId);
+        
+        verify(sessionRepository).save(session);
     }
 }
