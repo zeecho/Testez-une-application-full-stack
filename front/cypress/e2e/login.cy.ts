5dc78813f3613b2403c683aae37925acfb51976a
zecho <zeecho@users.noreply.github.com>
2024-04-12 01:47:51 +0200

    Add logout e2e test

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,5 +1,5 @@
 describe('Login spec', () => {
-  it('Login successfull', () => {
+  it('Login successful', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
@@ -23,5 +23,35 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  })
+
+  it('Logout successful', () => {
+    cy.visit('/login')
+
+    cy.intercept('POST', '/api/auth/login', {
+      body: {
+        id: 1,
+        username: 'userName',
+        firstName: 'firstName',
+        lastName: 'lastName',
+        admin: true
+      },
+    })
+
+    cy.intercept(
+      {
+        method: 'GET',
+        url: '/api/session',
+      },
+      []).as('session')
+
+    cy.get('input[formControlName=email]').type("yoga@studio.com")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
+
+    cy.url().should('include', '/sessions')
+
+    cy.get('.link').contains('Logout').click();
+    cy.url().should('not.include', '/sessions')
+    cy.get('span[routerlink="login"]').should('exist');
   })
 });
