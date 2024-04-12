c1af3ca656655114c850c88a0edf81f3cb8e3258
zecho <zeecho@users.noreply.github.com>
2024-04-12 02:04:40 +0200

    Add file with all tests

---
+++ front/cypress/e2e/all.cy.ts
@@ -0,0 +1,7 @@
+import './login.cy';
+import './meAsAdmin.cy';
+import './meAsUser.cy';
+import './register.cy';
+import './sessionsAsAdmin.cy';
+import './sessionsAsUser.cy';
+import './sessionsForEveryone.cy';
